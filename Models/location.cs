b068849a975485d4a2e06aa0b7c14337a51bded1
Louis Castillo <louiscastillo.dev@outlook.com>
2023-08-17 23:49:01 -0400

    added locations from ER Api and styled better

--- Models/location.cs
+++ Models/location.cs
@@ -1,10 +1,18 @@
 namespace EldenRingWiki.Models
 {
-public class Location{
-    public string ? Id { get; set; }
-    public string ? Name { get; set; }
-    public string ? Image { get; set; }
-    public string ? Description { get; set; }
-}
+    public class LocationResponse
+    {
+        public bool Success { get; set; }
+        public int Count { get; set; }
+        public int Total { get; set; }
+        public List<Location>? Data { get; set; }  // Change Npc to Location
+    }
 
+    public class Location
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Image { get; set; }
+        public string? Description { get; set; }
+    }
 }
--- Services/LocationService.cs
+++ Services/LocationService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using EldenRingWiki.Models;
@@ -9,20 +10,50 @@ namespace EldenRingWiki.Services
     public class LocationService
     {
         private readonly HttpClient _httpClient;
+        private readonly string _apiUrl = "https://eldenring.fanapis.com/api/locations"; // Ideally, fetch from configuration.
+        private readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
 
         public LocationService(HttpClient httpClient)
         {
             _httpClient = httpClient;
         }
 
-        public async Task<IEnumerable<Location>?> GetAllLocations()
+        public async Task<IEnumerable<Location>?> GetAlllocations()
         {
-            var response = await _httpClient.GetAsync("https://eldenring.fanapis.com/api/locations");
-
-            if (response.IsSuccessStatusCode)
+            try
             {
-                using var responseBody = await response.Content.ReadAsStreamAsync();
-                return await JsonSerializer.DeserializeAsync<IEnumerable<Location>>(responseBody);
+                var response = await _httpClient.GetAsync(_apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    if (response.Content.Headers.ContentType?.MediaType == "application/json")
+                    {
+                        using var responseBody = await response.Content.ReadAsStreamAsync();
+
+                        // Deserialize using the specified JSON options
+                        var locationResponse = await JsonSerializer.DeserializeAsync<LocationResponse>(responseBody, _jsonOptions);
+
+                        // Return the Data property which contains the locations
+                        return locationResponse?.Data;
+                    }
+                    else
+                    {
+                        throw new Exception("Unexpected content type received from the server.");
+                    }
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                // Log or handle the network-related exception here
+                throw new Exception($"Error fetching locations: {ex.Message}");
+            }
+            catch (JsonException ex)
+            {
+                // Handle JSON deserialization exception
+                throw new Exception($"Error deserializing location data: {ex.Message}");
             }
 
             return null;
